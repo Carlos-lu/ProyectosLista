072c9a54f6bf7300414d4c114398c0aa417536ec
Fernando10g97 <fernando10g97@gmail.com>
2019-12-05 11:03:22 -0500

    Agregar valores a una list

---
+++ ProyectosLista/Clases/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectosLista.Clases
+{
+    class Estudiante
+    {
+        public String NombreCompleto { get; set; }
+        public String Genero { get; set; }
+        public String EstadoCivil { get; set; }
+        public int edad { get; set; }
+
+    }
+}
